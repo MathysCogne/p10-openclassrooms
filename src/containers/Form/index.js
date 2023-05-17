20c20be6e92d13ca18cc50f4b26381319f38cd76
Mathys Cogne Foucault <mathys.cognef@gmail.com>
2023-05-17 22:39:34 +0200

    Message de confirmation lors de l'envoie du form

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -3,52 +3,62 @@ import PropTypes from "prop-types";
 import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button";
+import "./style.scss";
 
 const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
-  const sendContact = useCallback(
-    async (evt) => {
-      evt.preventDefault();
-      setSending(true);
-      // We try to call mockContactApi
-      try {
-        await mockContactApi();
-        setSending(false);
-      } catch (err) {
-        setSending(false);
-        onError(err);
-      }
-    },
-    [onSuccess, onError]
-  );
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
+  const sendContact = useCallback(async (evt) => {
+    evt.preventDefault();
+    setSending(true);
+
+    // We try to call mockContactApi
+    try {
+      await mockContactApi();
+      setSending(false);
+      setShowConfirmation(true);
+    } catch (err) {
+      setSending(false);
+      onError(err);
+    }
+  }, [onSuccess, onError]);
+
   return (
+    // Ajout d'une condition pour afficher le message de confirmation
     <form onSubmit={sendContact}>
-      <div className="row">
-        <div className="col">
-          <Field placeholder="" label="Nom" />
-          <Field placeholder="" label="Prénom" />
-          <Select
-            selection={["Personel", "Entreprise"]}
-            onChange={() => null}
-            label="Personel / Entreprise"
-            type="large"
-            titleEmpty
-          />
-          <Field placeholder="" label="Email" />
-          <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
-            {sending ? "En cours" : "Envoyer"}
-          </Button>
+      {showConfirmation ? (
+        <div className="message__cont">
+          <p className="message__text">Votre message a été envoyé avec succès, merci à très vite :)</p>
         </div>
-        <div className="col">
-          <Field
-            placeholder="message"
-            label="Message"
-            type={FIELD_TYPES.TEXTAREA}
-          />
+      ) : (
+        <div className="row">
+          <div className="col">
+            <Field placeholder="" label="Nom" />
+            <Field placeholder="" label="Prénom" />
+            <Select
+              selection={["Personel", "Entreprise"]}
+              onChange={() => null}
+              label="Personel / Entreprise"
+              type="large"
+              titleEmpty
+            />
+            <Field placeholder="" label="Email" />
+            <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
+              {sending ? "En cours" : "Envoyer"}
+            </Button>
+          </div>
+          <div className="col">
+            <Field
+              placeholder="message"
+              label="Message"
+              type={FIELD_TYPES.TEXTAREA}
+            />
+          </div>
         </div>
-      </div>
+      )}
     </form>
   );
 };
